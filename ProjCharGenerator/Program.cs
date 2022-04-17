30fa6d30553434534d1dca35aa97b53c88ef4e49
Rastuharem <Rastuharem@yandex.com>
2022-04-17 12:42:35 +0300

    Added files

--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -1,45 +1,171 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 
 namespace generator
 {
-    class CharGenerator 
+    public class BigrammGenerator
     {
-        private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя"; 
-        private char[] data;
-        private int size;
-        private Random random = new Random();
-        public CharGenerator() 
+        const string Alphabet = "абвгдежзийклмнопрстуфхцчшщыьэюя";
+        private char[] Letters;
+        private string PathToTable { get; }
+
+        public BigrammGenerator(string path)
         {
-           size = syms.Length;
-           data = syms.ToCharArray(); 
+            PathToTable = path;
+            Letters = Alphabet.ToCharArray();
         }
-        public char getSym() 
+
+        public int[][] PullBigrammFromFile(string path)
         {
-           return data[random.Next(0, size)]; 
+            string[] RawBigramm = File.ReadAllLines(path);
+            int[][] CookedBigramm = new int[RawBigramm.Length][];
+            for (int i = 0; i < CookedBigramm.Length; i++)
+            {
+                string[] str = RawBigramm[i].Split('\t');
+                CookedBigramm[i] = new int[str.Length];
+                for (int j = 0; j < str.Length; j++)
+                    CookedBigramm[i][j] = Convert.ToInt32(str[j]);
+            }
+            return CookedBigramm;
+        }
+        public char GetNextSymbol(char PrevSymbol)
+        {
+            int[][] Bigramm = PullBigrammFromFile(PathToTable);
+            int[] BigrammRow = new int[31];
+            int i = Alphabet.IndexOf(PrevSymbol);
+            for (int j = 0; j < Bigramm[i].Length; j++)
+                BigrammRow[j] = Bigramm[i][j];
+            Random rnd = new Random();
+            return Alphabet[rnd.Next(0, BigrammRow.Length - 1)];
+        }
+
+        public string GenerateText()
+        {
+            char[] Text = new char[1000];
+            Random rnd = new Random();
+            char PrevSymbol = Letters[rnd.Next(0, Letters.Length)];
+            Text[0] = PrevSymbol;
+            for (int i = 1; i < 1000; i++)
+            {
+                Text[i] = GetNextSymbol(PrevSymbol);
+                PrevSymbol = Text[i];
+            }
+            return new string(Text);
         }
     }
+
+    public class FreqGenerator
+    {
+        private string PathToTable { get; }
+
+        public FreqGenerator(string path)
+        {
+            PathToTable = path;
+        }
+
+        public (string[], int[]) PullFreqFromFile(string path)
+        {
+            string[] Words = new string[100];
+            string[] RawTable = File.ReadAllLines(path);
+            int[] Weights = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                Words[i] = RawTable[i].Split(" ")[0];
+                Weights[i] = int.Parse(RawTable[i].Split(" ")[1]);
+            }
+            return (Words, Weights);
+        }
+        public string GetNextWord(string PrevWord)
+        {
+            (string[] Words, int[] Weights) = PullFreqFromFile(PathToTable);
+            int i = Array.IndexOf(Words, PrevWord);
+            Random rnd = new Random();
+            int rnd_i = rnd.Next(95, 100);
+            if (i <= rnd_i)
+                return Words[i];
+            return Words[rnd_i];
+        }
+
+        public string GenerateText()
+        {
+            (string[] Words, int[] Weights) = PullFreqFromFile(PathToTable);
+            string[] Text = new string[1000];
+            string PrevWord = "";
+            Random rnd = new Random();
+            for (int i = 0; i < 1000; i++)
+            {
+                PrevWord = Words[rnd.Next(0, Words.Length)];
+                Text[i] = GetNextWord(PrevWord);
+            }
+            return string.Join(' ', Text);
+        }
+    }
+
+    public class BigrammFreqGenerator
+    {
+        private string PathToTable { get; }
+
+        public BigrammFreqGenerator(string path)
+        {
+            PathToTable = path;
+        }
+
+        public (string[], int[]) PullBigrammFromFile(string path)
+        {
+            string[] Words = new string[100];
+            string[] RawBigramm = File.ReadAllLines(path);
+            int[] Weights = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                Words[i] = RawBigramm[i].Split("  ")[0];
+                Weights[i] = int.Parse(RawBigramm[i].Split("  ")[1]);
+            }
+            return (Words, Weights);
+        }
+        public string GetNextWord(string PrevWord)
+        {
+            (string[] Words, int[] Weights) = PullBigrammFromFile(PathToTable);
+            int i = Array.IndexOf(Words, PrevWord);
+            Random rnd = new Random();
+            int rnd_i = rnd.Next(95, 100);
+            if (i <= rnd_i)
+                return Words[i];
+            return Words[rnd_i];
+        }
+
+        public string GenerateText()
+        {
+            (string[] Words, int[] Weigths) = PullBigrammFromFile(PathToTable);
+            Random rnd = new Random();
+            string[] Text = new string[1000];
+            string PrevWord = "";
+            for (int i = 0; i < 1000; i++)
+            {
+                PrevWord = Words[rnd.Next(0, Words.Length)];
+                Text[i] = GetNextWord(PrevWord);
+            }
+            return string.Join(' ', Text);
+        }
+    }
+
     class Program
     {
+        private static void PushTextToFile(string path, string str)
+        {
+            File.Create(path).Close();
+            File.WriteAllText(path, str);
+        }
+
         static void Main(string[] args)
         {
-            CharGenerator gen = new CharGenerator();
-            SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
-            for(int i = 0; i < 1000; i++) 
-            {
-               char ch = gen.getSym(); 
-               if (stat.ContainsKey(ch))
-                  stat[ch]++;
-               else
-                  stat.Add(ch, 1); Console.Write(ch);
-            }
-            Console.Write('\n');
-            foreach (KeyValuePair<char, int> entry in stat) 
-            {
-                 Console.WriteLine("{0} - {1}",entry.Key,entry.Value/1000.0); 
-            }
-            
+            BigrammGenerator bg = new BigrammGenerator("../../../../Tables/SymbolsBigramm.txt");
+            PushTextToFile("../../../../Results/result1.txt", bg.GenerateText());
+
+            FreqGenerator fg = new FreqGenerator("../../../../Tables/FreqGramm.txt");
+            PushTextToFile("../../../../Results/result2.txt", fg.GenerateText());
+
+            BigrammFreqGenerator bfg = new BigrammFreqGenerator("../../../../Tables/BiFreqGramm.txt");
+            PushTextToFile("../../../../Results/result3.txt", bfg.GenerateText());
         }
     }
 }
-
