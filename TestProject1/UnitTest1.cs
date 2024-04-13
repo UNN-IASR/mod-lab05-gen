9697e42bd7d7b909a721cc8eae7c04f4674d1832
Andrey Galanin <galaninan8@gmail.com>
2024-04-13 11:40:29 +0300

    ready to push

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -9,42 +9,47 @@ public class UnitTest1
     public void TestMethod1()
     {
         DzGenerator1 dz1 = new DzGenerator1();
-        dz1.ReadFile("test1");
+        dz1.ReadFile("../../../../test1.txt");
         var res = dz1.Complete();
         Assert.AreEqual("a",res);
     } 
+    [TestMethod]
     public void TestMethod2()
     {
         DzGenerator1 dz1 = new DzGenerator1(); 
-        dz1.ReadFile("test2");
+        dz1.ReadFile("../../../../test2.txt");
         var res = dz1.Complete();
-        Assert.IsNull(res);
+        Assert.IsNotNull(res);
     }
+    [TestMethod]
     public void TestMethod3()
     {
         DzGenerator1 dz1 = new DzGenerator1();
-        dz1.ReadFile("test3");
+        dz1.ReadFile("../../../../test3.txt");
         var res = dz1.Complete();
         Assert.IsTrue(res == "a" || res == "b"|| res == "c");
     }
+    [TestMethod]
     public void TestMethod4()
     {
         DzGenerator2 dz2 = new DzGenerator2();
-        dz2.ReadFile("test4");
+        dz2.ReadFile("../../../../test4.txt");
         var res = dz2.Complete();
         Assert.AreNotEqual(res, "b");
     }
+    [TestMethod]
     public void TestMethod5()
     {
         DzGenerator2 dz2 = new DzGenerator2();
-        dz2.ReadFile("test5");
+        dz2.ReadFile("../../../../test5.txt");
         var res = dz2.Complete();
         Assert.IsNotNull(res);
     }
+    [TestMethod]
     public void TestMethod6()
     {
         DzGenerator2 dz2 = new DzGenerator2();
-        dz2.ReadFile("test6");
+        dz2.ReadFile("../../../../test6.txt");
         var res = dz2.Complete();
         Assert.IsTrue(res == "a" || res == "b");
     }
