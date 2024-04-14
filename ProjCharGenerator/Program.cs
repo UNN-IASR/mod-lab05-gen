4e96e96d318714198b088e7bf0095477a5756a93
h1nekora <93097793+h1nekora@users.noreply.github.com>
2024-04-14 18:49:56 +0300

    Update Program.cs

--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -19,6 +19,41 @@ namespace generator
            return data[random.Next(0, size)]; 
         }
     }
+    public class Generator
+    {
+        private Random random = new Random();
+        private int[] weight;
+        private string[] words;
+        private int sum;
+        public Generator(string file)
+        {
+            sum = 0;
+            string[] linesStr=File.ReadAllLines(file);
+            int len=linesStr.Length;
+            this.words = new string[len];
+            this.weight = new int[len];
+            for(int i = 0; i < len; i++)
+            {
+                string[] split = linesStr[i].Split(' ');
+                this.words[i] = split[0];
+                sum += int.Parse(split[1]);
+                this.weight[i] = sum;
+            }
+        }
+        public string ResultText()
+        {
+            int rand = random.Next(0, sum);
+            for(int i = 0;i < words.Length;i++)
+            {
+                if (rand < weight[i])return words[i];
+            }
+            return "";
+        }
+        public int getSum()
+        {
+            return sum;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -38,7 +73,21 @@ namespace generator
             {
                  Console.WriteLine("{0} - {1}",entry.Key,entry.Value/1000.0); 
             }
-            
+            Generator generator=new Generator("bigram_in.txt");
+            string result_bigram = "";
+            string result_words = "";
+            for(int i = 0; i < 500; i++)
+            {
+                result_bigram += generator.ResultText();
+            }
+            File.WriteAllText("gen-1.txt", result_bigram);
+            Generator generator1 = new Generator("words_in.txt");
+            for(int i = 0;i < 1000; i++)
+            {
+                result_words += generator1.ResultText();
+                result_words += " ";
+            }
+            File.WriteAllText("gen-2.txt", result_words);
         }
     }
 }
