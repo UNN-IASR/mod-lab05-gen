1f4613522892653088da1a4c3e278051c8b4e02a
MichailImchikov <93055597+MichailImchikov@users.noreply.github.com>
2024-04-21 22:24:25 +0300

    Update Laba5Tests.cs

--- ProjCharGenerator.Tests/Laba5Tests.cs
+++ ProjCharGenerator.Tests/Laba5Tests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using laba5;
 using System;
 using System.Collections.Generic;
@@ -8,22 +8,43 @@ using System.Threading.Tasks;
 
 namespace laba5.Tests
 {
-    [TestClass]
-    public class CreatingTextTests
+    [TestClass()]
+    public class Laba5Test
     {
-        [TestMethod]
+        [TestMethod()]
         public void CreatingTextTest()
         {
             CreatingText text = new("Probability.txt");
             Assert.AreEqual(text.sum, 414975024);
-            //Assert.Fail();
         }
-        [TestMethod]
+        [TestMethod()]
         public void CreatingTextTest1()
         {
             CreatingText text = new("Probability.txt");
-            Assert.AreEqual(text.sum, 414975024);
-            //Assert.Fail();
+            Assert.AreEqual(text.Power[1], 654430);
+        }
+        [TestMethod()]
+        public void CreatingTextTest2()
+        {
+            CreatingText text = new("Probability.txt");
+            Assert.AreEqual(text.words[1], "аб");
+        }
+        [TestMethod()]
+        public void CreatingTextTest3()
+        {
+            CreatingText text = new("Probability.txt");
+            Assert.AreEqual(text.Power.Count, 694);
+        }
+        [TestMethod()]
+        public void CreatingTextTest4()
+        {
+            CreatingText text = new("Probability.txt");
+            Assert.AreEqual(text.Power[0], 8146);
+        }
+        public void CreatingTextTest5()
+        {
+            CreatingText text = new("Probability.txt");
+            Assert.AreEqual(text.words[0], "aa");
         }
     }
 }
