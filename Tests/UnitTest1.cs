b62a75508bb6dc696c95a2ad5c339188213baf41
h1nekora <93097793+h1nekora@users.noreply.github.com>
2024-04-22 00:19:04 +0300

    Update UnitTest1.cs

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -16,31 +16,31 @@ namespace TestProject1
         [TestMethod]
         public void TestMethod2()
         {
-            Generator generator = new Generator("bigram_in.txt");
+            Generator generator = new Generator("../../../../bigram_in.txt");
             Assert.IsNotNull(generator);
         }
         [TestMethod]
         public void TestMethod3()
         {
-            Generator bigram = new Generator("bigram_in.txt");
+            Generator bigram = new Generator("../../../../bigram_in.txt");
             Assert.IsTrue(bigram.getWeight(0) == 8146);
         }
         [TestMethod]
         public void TestMethod4()
         {
-            Generator generator = new Generator("bigram_in.txt");
+            Generator generator = new Generator("../../../../bigram_in.txt");
             Assert.IsTrue(generator.getSum() > 0);
         }
         [TestMethod]
         public void TestMethod5()
         {
-            Generator words = new Generator("words_in.txt");
+            Generator words = new Generator("../../../../words_in.txt");
             Assert.IsTrue(words.getWord(0) == "и");
         }
         [TestMethod]
         public void TestMethod6()
         {
-            Generator bigram = new Generator("bigram_in.txt");
+            Generator bigram = new Generator("../../../../bigram_in.txt");
             Assert.IsTrue(bigram.getWeight(1) == 654430);
         }
     }
