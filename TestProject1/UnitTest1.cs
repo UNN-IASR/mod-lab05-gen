2a71761313fe278797dec70a3f2517420a09cb9a
Анненко Миша 3821Б1ПИпр1 <146870367+AnnenkoMisha3821B1PIpr1@users.noreply.github.com>
2024-05-15 01:51:27 +0300

    Add files via upload

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,54 +1,59 @@
-namespace ProjCharGenerator.Tests;
 using generator;
-
-[TestMethod]
-public void Test_1_1()
+namespace TestProject1
 {
-    CharGenerator gen0 = new CharGenerator();
-    string alf = "–∞–±–≤–≥–¥–µ—ë–∂–∑–∏–π–∫–ª–º–Ω–æ–ø—Ä—Å—Ç—É—Ñ—Ö—Ü—á—à—â—å—ã—ä—ç—é—è";
-
-    for (int i = 0; i < 100; i++)
+    [TestClass]
+    public class UnitTest1
     {
-        Assert.IsFalse(!alf.Contains(gen0.getSym()));
+        [TestMethod]
+        public void Test_1_1()
+        {
+            CharGenerator gen0 = new CharGenerator();
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.IsFalse(string.IsNullOrEmpty(string.Concat(gen0.getSym())));
+            }
+        }
+
+        [TestMethod]
+        public void Test_1_2()
+        {
+            CharGenerator gen0 = new CharGenerator();
+            string alf = "‡·‚„‰Â∏ÊÁËÈÍÎÏÌÓÔÒÚÛÙıˆ˜¯˘¸˚˙˝˛ˇ";
+
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.IsFalse(!alf.Contains(gen0.getSym()));
+            }
+        }
+
+        [TestMethod]
+        public void Test_2_1()
+        {
+            Generator_BIgramm gen1 = new Generator_BIgramm();
+            Assert.IsFalse(string.IsNullOrEmpty(string.Concat(gen1.GetSymbol(' '))));
+        }
+
+        [TestMethod]
+        public void Test_2_2()
+        {
+            Generator_BIgramm gen1 = new Generator_BIgramm();
+            string alf = "‡·‚„‰Â∏ÊÁËÈÍÎÏÌÓÔÒÚÛÙıˆ˜¯˘¸˚˙˝˛ˇ";
+            Assert.IsFalse(!alf.Contains(gen1.GetSymbol(' ')));
+        }
+
+        [TestMethod]
+        public void Test_3_1()
+        {
+            Generator_Words_Text gen2 = new Generator_Words_Text();
+            gen2.CalculateAndMinimizeSum();
+            Assert.AreEqual(gen2.totalSum, gen2.upperBounds[99]);
+        }
+
+        [TestMethod]
+        public void Test_3_2()
+        {
+            Generator_Words_Text gen2 = new Generator_Words_Text();
+            Assert.AreEqual(gen2.wordList[6], "·˚Ú¸");
+        }
     }
-}
-
-[TestMethod]
-public void Test_1_2()
-{
-    CharGenerator gen0 = new CharGenerator();
-    for (int i = 0; i < 100; i++)
-    {
-        Assert.IsFalse(string.IsNullOrEmpty(string.Concat(gen0.getSym())));
-    }
-}
-
-[TestMethod] 
-public void Test_2_1()
-{
-    Generator_BIgramm gen1 = new Generator_BIgramm();
-    string alf = "–∞–±–≤–≥–¥–µ—ë–∂–∑–∏–π–∫–ª–º–Ω–æ–ø—Ä—Å—Ç—É—Ñ—Ö—Ü—á—à—â—å—ã—ä—ç—é—è";
-    Assert.IsFalse(!alf.Contains(gen1.getSym(' ')));
-}
-
-[TestMethod]
-public void Test_2_2()
-{
-    Generator_BIgramm gen1 = new Generator_BIgramm();
-    Assert.IsFalse(string.IsNullOrEmpty(string.Concat(gen1.getSym(' '))));
-}
-
-[TestMethod]
-public void Test_3_1()
-{
-    Generator_Words_Text gen2 = new Generator_Words_Text();
-    gen2.Minimizing_and_Calculating_Summa();
-    Assert.AreEqual(gen2.Summa, gen2.np[99]);
-}
-
-[TestMethod]
-public void Test_3_2()
-{
-    Generator_Words_Text gen2 = new Generator_Words_Text();
-    Assert.AreEqual(gen2.words[1], "–≤");
 }
