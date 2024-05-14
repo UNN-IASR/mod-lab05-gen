e06778dc1ca7a7e6bfc9579cea49991cfa2191b7
Анненко Миша 3821Б1ПИпр1 <146870367+AnnenkoMisha3821B1PIpr1@users.noreply.github.com>
2024-05-15 01:52:41 +0300

    Add files via upload

--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -1,191 +1,174 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
 namespace generator
 {
-    class Program
+    public class CharGenerator
     {
-        public class CharGenerator
+        private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
+        private char[] data;
+        private int size;
+        private Random random = new Random();
+        public CharGenerator()
         {
-            private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
-            private char[] data;
-            private int size;
-            private Random random = new Random();
-            public CharGenerator()
+            size = syms.Length;
+            data = syms.ToCharArray();
+        }
+        public char getSym()
+        {
+            return data[random.Next(0, size)];
+        }
+    }
+
+    public class Generator_BIgramm
+    {
+        private string alphabet = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
+        private char[] characters;
+        private int sequenceLength;
+        private Random generator = new Random();
+        private int[,] boundaries;
+        private int[,] probabilities;
+
+        public Generator_BIgramm()
+        {
+            sequenceLength = alphabet.Length;
+            characters = alphabet.ToCharArray();
+            boundaries = new int[sequenceLength, sequenceLength];
+            probabilities = new int[sequenceLength, sequenceLength];
+
+            string fileContent = File.ReadAllText(@"C:\Users\wizze\source\repos\ProjCharGenerator\ProjCharGenerator\BI.txt");
+            string[] lines = fileContent.Split('\n');
+            foreach (string line in lines)
             {
-                size = syms.Length;
-                data = syms.ToCharArray();
+                if (!string.IsNullOrEmpty(line))
+                {
+                    string[] parts = line.Split(' ');
+                    int weight = int.Parse(parts[2]);
+                    probabilities[alphabet.IndexOf(parts[1][0]), alphabet.IndexOf(parts[1][1])] = weight;
+                }
             }
-            public char getSym()
+
+            int totalWeight = 0;
+            for (int i = 0; i < sequenceLength; i++)
             {
-                return data[random.Next(0, size)];
+                for (int j = 0; j < sequenceLength; j++)
+                {
+                    totalWeight += probabilities[i, j];
+                    boundaries[i, j] = totalWeight;
+                }
             }
         }
-        
-        
-        public class Generator_BIgramm
+
+        public char GetSymbol(char input)
         {
-            private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
-            private char[] data;
-            private int size;
-            private Random random = new Random();
-            //Массив верхних границ диапазона целых чисел для каждого из символов
-            private int[,] np;
-            // Массив весов
-            private int[,] weights;
-            public Generator_BIgramm()
+            int index = 0;
+            if (alphabet.Contains(input))
             {
-                size = syms.Length;
-                data = syms.ToCharArray();
-                np = new int[size, size];
-                weights = new int[size, size];
-        
-                string BItxt = @"C:\Users\wizze\source\repos\lab5MIPS\txt files for generate\BI.txt";
-                StreamReader Stream_Reader = new StreamReader(BItxt, Encoding.GetEncoding(1251));
-        
-                //Считываем первую строку из файла BI.txt
-                #nullable enable
-                string? line = Stream_Reader.ReadLine();
-                //Продолжаем читать файл BI.txt пока не закончится 
-                while (line != null)
+                int lowerBound;
+                if (input != alphabet[0])
                 {
-                    string[] Split_line = line.Split(new char[] { '\t' });
-                    int W = int.Parse(Split_line[2]);
-                    weights[syms.IndexOf(Split_line[1][0]), syms.IndexOf(Split_line[1][1])] = W;
-                    line = Stream_Reader.ReadLine();
-                }
-        
-                int summary_weigt = 0;
-                //Заполнение массива верхних границ символов
-                for (int i = 0; i < size; i++)
-                {
-                    for (int j = 0; j < size; j++)
-                    {
-                        summary_weigt += weights[i, j];
-                        np[i, j] = summary_weigt;
-                    }
-                }
-            }
-        
-            public char getSym(char letter)
-            {
-                //m - это случайное число получаемое в диапазоне от l до r
-                int m = 0;
-                if (syms.Contains(letter))
-                {
-                    int l;
-                    if (letter != syms[0])
-                    {
-                        l = np[syms.IndexOf(letter) - 1, size - 1];
-                    }
-                    else
-                    {
-                        l = 0;
-                    }
-                    int r = np[syms.IndexOf(letter), size - 1];
-                    m = random.Next(l, r + 1);
+                    lowerBound = boundaries[alphabet.IndexOf(input) - 1, sequenceLength - 1];
                 }
                 else
                 {
-                    return syms[random.Next(0, size)];
+                    lowerBound = 0;
                 }
-        
-                int j = 0;
-                //Поиск символа по его диапазону случайных значений
-                for (int i = 0; i < size; i++)
+                int upperBound = boundaries[alphabet.IndexOf(input), sequenceLength - 1];
+                int randomIndex = generator.Next(lowerBound, upperBound + 1);
+
+                for (int i = 0; i < sequenceLength; i++)
                 {
-                    for (j = 0; j < size; j++)
+                    for (int j = 0; j < sequenceLength; j++)
                     {
-                        if (m <= np[i, j])
+                        if (randomIndex <= boundaries[i, j])
                         {
-                            return data[j];
+                            return characters[j];
                         }
                     }
                 }
-                return data[j];
             }
-        }
-        
-        public class Generator_Words_Text
-        {
-            private const int limit = 100;
-            //По заданию разрешено взять 100 слов и их частотных свойств
-            private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
-            private int size;
-            private Random random = new Random();
-            //Слова
-            public string[] words;
-            //Частоты использования слов
-            public int[] frequency;
-            //Массив верхних границ диапазона целых чисел
-            public int[] np;
-            public int Summa = 0;
-        
-            public Generator_Words_Text()
+            else
             {
-                size = syms.Length;
-                np = new int[limit];
-                words = new string[limit];
-                frequency = new int[limit];
-        
-                string FreqWords = @"C:\Users\wizze\source\repos\lab5MIPS\txt files for generate\frequency_properties_of_words.txt";
-                StreamReader Stream_Reader = new StreamReader(FreqWords, Encoding.GetEncoding(1251));
-        
-                //Считываем первую строку из файла requency_properties_of_words.txt
-                #nullable enable
-                string? line = Stream_Reader.ReadLine();
-                //Продолжаем читать файл frequency_properties_of_words.txt пока не закончится 
-                int i = 0;
+                return characters[generator.Next(0, sequenceLength)];
+            }
+
+            return characters[index];
+        }
+    }
+
+    public class Generator_Words_Text
+    {
+        private const int maxWordsCount = 100;
+        private string alphabet = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
+        private int sequenceLength;
+        private Random generator = new Random();
+        public string[] wordList;
+        public int[] wordFrequencies;
+        public int[] upperBounds;
+        public int totalSum = 0;
+
+        public Generator_Words_Text()
+        {
+            sequenceLength = alphabet.Length;
+            upperBounds = new int[maxWordsCount];
+            wordList = new string[maxWordsCount];
+            wordFrequencies = new int[maxWordsCount];
+
+            string freqWordsPath = @"C:\Users\wizze\source\repos\ProjCharGenerator\ProjCharGenerator\frequency_properties_of_words.txt";
+            using (StreamReader streamReader = new StreamReader(freqWordsPath))
+            {
+                string? line = streamReader.ReadLine();
+                int currentIndex = 0;
                 while (line != null)
                 {
-                    string[] Split_line = line.Split(new char[] { '\t' });
-                    // index 1 = word; index 3 = frequency
-                    words[i] = Split_line[1];
-                    frequency[i] = int.Parse(Split_line[3]);
-                    i++;
-                    line = Stream_Reader.ReadLine();
+                    string[] splitLine = line.Split('\t');
+                    wordList[currentIndex] = splitLine[1];
+                    wordFrequencies[currentIndex] = int.Parse(splitLine[3]);
+                    currentIndex++;
+                    line = streamReader.ReadLine();
                 }
-            }
-        
-            //Чтобы избежать работу с миллионами, для удобства уменьшим частоты
-            //Берём самую минимальную частоту MIN и вычитаем её из каждого элемента
-            public void Minimizing_and_Calculating_Summa()
-            {
-                int min = frequency[99];
-                for (int i = 0; i < 100; i++)
-                {
-                    frequency[i] -= min;
-                }
-                //Считаем summa опираясь на верхние границы из np
-                np[0] = frequency[0];
-                for (int i = 1; i < 100; i++)
-                {
-                    np[i] = np[i - 1] + frequency[i];
-                }
-                Summa = np[99];
-            }
-        
-            public string getWord()
-            {
-                int m = random.Next(0, Summa);
-                int j = 0;
-                for (j = 0; j < size; j++)
-                {
-                    if (m < np[j])
-                    {
-                        break;
-                    }
-                }
-                return words[j];
             }
         }
-        
+
+        public void CalculateAndMinimizeSum()
+        {
+            int minValue = wordFrequencies[maxWordsCount - 1];
+            for (int i = 0; i < maxWordsCount; i++)
+            {
+                wordFrequencies[i] -= minValue;
+            }
+            upperBounds[0] = wordFrequencies[0];
+            for (int i = 1; i < maxWordsCount; i++)
+            {
+                upperBounds[i] = upperBounds[i - 1] + wordFrequencies[i];
+            }
+            totalSum = upperBounds[maxWordsCount - 1];
+        }
+
+        public string GetRandomWord()
+        {
+            int randomIndex = generator.Next(0, totalSum);
+            int index = 0;
+            for (; index < sequenceLength; index++)
+            {
+                if (randomIndex < upperBounds[index])
+                {
+                    break;
+                }
+            }
+            return wordList[index];
+        }
+    }
+
+    class Program
+    {
         static void funtion_for_gen_0_txt()
         {
             //Записываем путь файла gen-0.txt для удобства работы с ним
-            string File_Name = @"C:\Users\wizze\source\repos\lab5MIPS\gen-0.txt";
+            //C:\Users\wizze\source\repos\ProjCharGenerator
+            string File_Name = @"C:\Users\wizze\source\repos\ProjCharGenerator\gen-0.txt";
             //Записываем всё в файл gen-0.txt в результате работы CharGenerator
             CharGenerator gen = new CharGenerator();
             using (StreamWriter Stream_Writer = new StreamWriter(File_Name))
@@ -215,9 +198,10 @@ namespace generator
         static void funtion_for_gen_1_txt()
         {
             //Записываем путь файла gen-0.txt для удобства работы с ним
-            string File_Name = @"C:\Users\wizze\source\repos\lab5MIPS\gen-1.txt";
+            string File_Name = @"C:\Users\wizze\source\repos\ProjCharGenerator\gen-1.txt";
             //Записываем всё в файл gen-0.txt в результате работы CharGenerator
             Generator_BIgramm gen1 = new Generator_BIgramm();
+            //CharacterSequenceGenerator gen1 = new CharacterSequenceGenerator();
             //Первое вхождение для того чтобы оно работало 
             char letter = 'а';
             using (StreamWriter Stream_Writer = new StreamWriter(File_Name))
@@ -225,7 +209,7 @@ namespace generator
                 SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
                 for (int i = 0; i < 1000; i++)
                 {
-                    char ch = gen1.getSym(letter);
+                    char ch = gen1.GetSymbol(letter);
                     if (stat.ContainsKey(ch))
                     {
                         stat[ch]++;
@@ -248,15 +232,17 @@ namespace generator
         static void funtion_for_gen_2_txt()
         {
             //Записываем путь файла gen-0.txt для удобства работы с ним
-            string File_Name = @"C:\Users\wizze\source\repos\lab5MIPS\gen-2.txt";
+            string File_Name = @"C:\Users\wizze\source\repos\ProjCharGenerator\gen-2.txt";
             //Записываем всё в файл gen-0.txt в результате работы CharGenerator
             Generator_Words_Text gen2 = new Generator_Words_Text();
-            gen2.Minimizing_and_Calculating_Summa();
+            //gen2.Minimizing_and_Calculating_Summa();
+            gen2.CalculateAndMinimizeSum();
             using (StreamWriter Stream_Writer = new StreamWriter(File_Name))
             {
                 for (int i = 0; i < 1000; i++)
                 {
-                    string ch = String.Concat(gen2.getWord()," ");
+                    //string ch = String.Concat(gen2.getWord(), " ");
+                    string ch = String.Concat(gen2.GetRandomWord(), " ");
                     Stream_Writer.Write(ch);
                 }
             }
@@ -264,16 +250,12 @@ namespace generator
 
         static void Main(string[] args)
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            var enc1251 = Encoding.GetEncoding(1251);
-            System.Console.OutputEncoding = System.Text.Encoding.UTF8;
-            System.Console.InputEncoding = enc1251;
+            //funtion_for_gen_0_txt();
 
-            funtion_for_gen_0_txt();
-
-            funtion_for_gen_1_txt();
+            //funtion_for_gen_1_txt();
 
             funtion_for_gen_2_txt();
         }
     }
+    
 }
