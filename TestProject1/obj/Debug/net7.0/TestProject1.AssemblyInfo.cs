7585c672b017f4b77021693a24e35dbad003d4f6
ProWega <deriswerderis@gmail.com>
2024-05-26 20:31:11 +0300

    Тесты Net7.0

--- TestProject1/obj/Debug/net7.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net7.0/TestProject1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ProjCharGenerator")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a3139ff1652450650fc495faf3f2883d2703945")]
-[assembly: System.Reflection.AssemblyProductAttribute("ProjCharGenerator")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ProjCharGenerator")]
+[assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
