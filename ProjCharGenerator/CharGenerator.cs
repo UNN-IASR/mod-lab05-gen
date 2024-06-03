7b2b4f17ab5f04e5d9ad38886f5b07c59a969e4a
Nikita Parshutin <niki.saransk2016@gmail.com>
2024-06-03 11:17:28 +0300

    added 3 generators with 3 gens

---
+++ ProjCharGenerator/CharGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+using System.IO;
+
+namespace ProjCharGenerator
+{
+    public class CharGenerator
+    {
+        private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
+        private char[] data;
+        private int size;
+        private Random random = new Random();
+        public CharGenerator()
+        {
+            size = syms.Length;
+            data = syms.ToCharArray();
+        }
+        public char getSym()
+        {
+            return data[random.Next(0, size)];
+        }
+
+        public void Generate(int n)
+        {
+            using (StreamWriter writer = new StreamWriter("../../../gen0.txt", false))
+            {
+                SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
+            for (int i = 0; i < n; i++)
+            {
+                char ch = getSym();
+                if (stat.ContainsKey(ch))
+                    stat[ch]++;
+                else
+                    stat.Add(ch, 1);
+                writer.Write(ch);
+                Console.Write(ch);
+            }
+            Console.Write('\n');
+            foreach (KeyValuePair<char, int> entry in stat)
+            {
+                Console.WriteLine("{0} - {1}", entry.Key, entry.Value / 1000.0);
+            }
+            }
+        }
+    }
+}
---
+++ ProjCharGenerator/WordGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjCharGenerator
+{
+    public class WordGenerator
+    {
+        SortedDictionary<string, int> stat;
+
+        Int64 sum;
+
+        public WordGenerator()
+        {
+            stat = new SortedDictionary<string, int>();
+
+            string path = "../../../freqwords.txt";
+
+            // асинхронное чтение
+            using (StreamReader reader = new StreamReader(path, Encoding.UTF8))
+            {
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] words = line.Split(" ");
+                    stat.Add(words[1], int.Parse(words[2]));
+                    sum += int.Parse(words[2]);
+                }
+            }
+        }
+
+        public void Generate(int n)
+        {
+
+            var result = new StringBuilder();
+            var rand = new Random();
+            var prev = " ";
+            for (int i = 0; i < n; i++)
+            {
+                double s = 0;
+                double shot = (double)(rand.NextInt64(sum)) / sum;
+             
+                    result.Append(' ');
+               
+                    foreach (var item in stat)
+                    { 
+                        s += (double)item.Value / sum;
+                        if (s >= shot)
+                        {
+                            result.Append(item.Key);
+                            break;
+                        }
+                    }
+              
+                prev = result[result.Length - 1].ToString();
+            }
+
+            using (StreamWriter writer = new StreamWriter("../../../gen2.txt", false))
+            {
+                writer.Write(result);
+            }
+        }
+
+    }
+}
