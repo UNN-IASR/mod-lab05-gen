02c9955a6ae876bd2a252e5294f92a69fdae52b5
Shalnov.Yuri.Pipr2.3Kurs <96010611+jra1251@users.noreply.github.com>
2024-06-08 02:03:37 +0300

    Add files via upload

---
+++ ProjCharGenerator/Generator_BIGRAMM.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjCharGenerator
+{
+    public class Generator_BIGRAMM
+    {
+
+        private string alphabet = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя";
+        private char[] characters;
+        private int arraySize;
+        private Random generator = new Random();
+
+        // Массив верхних границ диапазона целых чисел для каждого из символов
+        int[,] upperLimits;
+        int[,] probabilities;
+
+        public Generator_BIGRAMM()
+        {
+            arraySize = alphabet.Length;
+            characters = alphabet.ToCharArray();
+            upperLimits = new int[arraySize, arraySize];
+            probabilities = new int[arraySize, arraySize];
+
+            string pathToFile = "../../../TextFile1.txt";
+
+            using (StreamReader fileReader = new StreamReader(pathToFile))
+            {
+                string? currentLine = fileReader.ReadLine();
+                while (currentLine != null)
+                {
+                    string[] splitLine = currentLine.Split(new char[] { '\t' });
+                    int probabilityValue = int.Parse(splitLine[2]);
+                    probabilities[alphabet.IndexOf(splitLine[1][0]), alphabet.IndexOf(splitLine[1][1])] = probabilityValue;
+                    currentLine = fileReader.ReadLine();
+                }
+            }
+
+            int totalProbabilitySum = 0;
+            for (int i = 0; i < arraySize; i++)
+            {
+                for (int j = 0; j < arraySize; j++)
+                {
+                    totalProbabilitySum += probabilities[i, j];
+                    upperLimits[i, j] = totalProbabilitySum;
+                }
+            }
+        }
+
+        public char GenerateNextSymbol(char currentLetter)
+        {
+            int index = 0;
+            if (alphabet.Contains(currentLetter))
+            {
+                int lowerBound;
+                if (currentLetter != alphabet[0])
+                {
+                    lowerBound = upperLimits[alphabet.IndexOf(currentLetter) - 1, arraySize - 1];
+                }
+                else
+                {
+                    lowerBound = 0;
+                }
+                int upperBound = upperLimits[alphabet.IndexOf(currentLetter), arraySize - 1];
+                index = generator.Next(lowerBound, upperBound + 1);
+            }
+            else
+            {
+                return alphabet[generator.Next(0, arraySize)];
+            }
+
+            int columnIndex = 0;
+            // Поиск следующего символа по его диапазону случайных значений
+            for (int rowIndex = 0; rowIndex < arraySize; rowIndex++)
+            {
+                for (columnIndex = 0; columnIndex < arraySize; columnIndex++)
+                {
+                    if (index <= upperLimits[rowIndex, columnIndex])
+                    {
+                        return characters[columnIndex];
+                    }
+                }
+            }
+            return characters[columnIndex];
+        }
+
+
+    }
+}
--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -1,45 +1,131 @@
-﻿using System;
+﻿using ProjCharGenerator;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
 
 namespace generator
 {
-    class CharGenerator 
+
+   public class Program
     {
-        private string syms = "абвгдеёжзийклмнопрстуфхцчшщьыъэюя"; 
-        private char[] data;
-        private int size;
-        private Random random = new Random();
-        public CharGenerator() 
+        
+        public static void Function_generation_onegramm()
         {
-           size = syms.Length;
-           data = syms.ToCharArray(); 
+            string Out_file = "../../GEN-ONEGRAMM.txt";
+            CharGenerator CG = new CharGenerator();
+            using (StreamWriter Stream_Writer = new StreamWriter(Out_file))
+            {
+                SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
+                for (int i = 0; i < 1000; i++)
+                {
+                    char ch = CG.getSym();
+                    if (stat.ContainsKey(ch))
+                    {
+                        stat[ch]++;
+                    }
+                    else
+                    {
+                        stat.Add(ch, 1);
+                    }
+                    
+                    Stream_Writer.Write(ch);
+                    if (i % 20 == 0 && i != 0) { Stream_Writer.WriteLine(); }//для комфортного чтения файла 
+                }
+                Stream_Writer.WriteLine();
+                foreach (KeyValuePair<char, int> entry in stat)
+                {
+                    Stream_Writer.WriteLine("{0} - {1}", entry.Key, entry.Value / 1000.0);
+                }
+            }
         }
-        public char getSym() 
+        public static void function_for_generation_bigramm()
+            {
+                string Out_file = "../../GEN-BI.txt";
+                //Записываем всё в файл gen-0.txt в результате работы CharGenerator
+                Generator_BIGRAMM bi = new Generator_BIGRAMM();
+                char Start_symbol = 'a';
+                using (StreamWriter Stream_Writer = new StreamWriter(Out_file))
+                {
+                    SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        char ch = bi.GenerateNextSymbol(Start_symbol);
+                        if (stat.ContainsKey(ch))
+                        {
+                            stat[ch]++;
+                        }
+                        else
+                        {
+                            stat.Add(ch, 1);
+                        }
+                        Start_symbol = ch;
+                        Stream_Writer.Write(ch);
+                    if (i % 20 == 0 && i != 0) { Stream_Writer.WriteLine(); }//для комфортного чтения файла 
+                }
+                    Stream_Writer.WriteLine();
+                    foreach (KeyValuePair<char, int> entry in stat)
+                    {
+                        Stream_Writer.WriteLine("{0} - {1}", entry.Key, entry.Value / 1000.0);
+                    }
+                }
+            }
+        public static void Func_for_generation_word()
         {
-           return data[random.Next(0, size)]; 
+            string Out_file = @"C:\Users\Юрий\source\repos\mod-lab05-gen\GEN-Words.txt";
+            //Записываем всё в файл gen-0.txt в результате работы CharGenerator
+            WordGenerator gw = new WordGenerator();
+            using (StreamWriter Stream_Writer = new StreamWriter(Out_file))
+            {
+                SortedDictionary<string, int> stat = new SortedDictionary<string, int>();
+                for (int i = 0; i < 1000; i++)
+                {
+                    
+                    string word = gw.GenerateWord();
+                    if (stat.ContainsKey(word))
+                    {
+                        stat[word]++;
+                    }
+                    else
+                    {
+                        stat.Add(word, 1);
+                    }
+                    Stream_Writer.Write(word + " ");
+                    if (i % 20 == 0&& i!=0) { Stream_Writer.WriteLine(); }//для комфортного чтения файла 
+                }
+                Stream_Writer.WriteLine();
+                foreach (KeyValuePair<string, int> entry in stat)
+                {
+                    Stream_Writer.WriteLine("{0} - {1}", entry.Key, entry.Value / 1000.0);
+                }
+            }
+           
         }
-    }
-    class Program
-    {
+    
         static void Main(string[] args)
         {
             CharGenerator gen = new CharGenerator();
             SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
-            for(int i = 0; i < 1000; i++) 
+            for (int i = 0; i < 1000; i++)
             {
-               char ch = gen.getSym(); 
-               if (stat.ContainsKey(ch))
-                  stat[ch]++;
-               else
-                  stat.Add(ch, 1); Console.Write(ch);
+                char ch = gen.getSym();
+                if (stat.ContainsKey(ch))
+                    stat[ch]++;
+                else
+                    stat.Add(ch, 1); Console.Write(ch);
             }
             Console.Write('\n');
-            foreach (KeyValuePair<char, int> entry in stat) 
+            foreach (KeyValuePair<char, int> entry in stat)
             {
-                 Console.WriteLine("{0} - {1}",entry.Key,entry.Value/1000.0); 
+                Console.WriteLine("{0} - {1}", entry.Key, entry.Value / 1000.0);
             }
-            
+
+            Function_generation_onegramm();
+            function_for_generation_bigramm();
+            Func_for_generation_word();
         }
     }
 }
+
 
