611db1aa8724048fdd9eaafdb1671b047e063ac3
Shalnov.Yuri.Pipr2.3Kurs <96010611+jra1251@users.noreply.github.com>
2024-06-08 02:46:07 +0300

    updates

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,62 @@
+using ProjCharGenerator;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void Test_1_1()
+        {
+            CharGenerator CG = new CharGenerator();
+            string alf = "אבגדהו¸זחטיךכלםמןנסעףפץצקרשüûת‎‏ÿ";
+
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.IsFalse(!alf.Contains(CG.getSym()));
+            }
+        }
+
+        [TestMethod]
+        public void Test_1_2()
+        {
+            CharGenerator CG = new CharGenerator();
+            for (int i = 0; i < 100; i++)
+            {
+                Assert.IsFalse(string.IsNullOrEmpty(string.Concat(CG.getSym())));
+            }
+        }
+
+        [TestMethod]
+        public void Test_2_1()
+        {
+            Generator_BIGRAMM BI = new Generator_BIGRAMM();
+            string alf = "אבגדהו¸זחטיךכלםמןנסעףפץצקרשüûת‎‏ÿ";
+            Assert.IsFalse(!alf.Contains(BI.GenerateNextSymbol(' ')));
+        }
+
+        [TestMethod]
+        public void Test_2_2()
+        {
+            Generator_BIGRAMM BI = new Generator_BIGRAMM();
+            Assert.IsFalse(string.IsNullOrEmpty(string.Concat(BI.GenerateNextSymbol(' '))));
+        }
+
+        [TestMethod]
+        public void Test_3_1()
+        {
+            WordGenerator gW2 = new WordGenerator();
+
+            Assert.IsTrue(!gW2.frequentWordsList.Contains("ךנמגאעü"));
+        }
+
+        [TestMethod]
+        public void Test_3_2()
+        {
+            WordGenerator gW2 = new WordGenerator();
+            Assert.AreEqual(gW2.frequentWordsList[1], "ג");
+        }
+        
+    }
+}
