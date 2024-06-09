33fa3afdfcecabef6d49e545513bc6b2f7d4bdde
kateandrianova <147188563+kateandrianova@users.noreply.github.com>
2024-06-09 18:38:42 +0300

    Update Test.cs

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -20,16 +20,5 @@ namespace generator
             TextGenerator generator = new TextGenerator(words, values);
             Assert.IsNotNull(generator);
         }
-
-        [TestMethod]
-        public void TestGetSymMethod()
-        {
-            string[] words = { "a", "b", "c", "d", "e", "f" };
-            double[] values = { 0.1, 0.2, 0.3, 0.6, 0.8, 1.0 };
-            TextGenerator generator = new TextGenerator(words, values);
-
-            string sym = generator.getSym();
-            Assert.IsTrue(sym == "a" || sym == "b" || sym == "c" || sym == "d" || sym == "e" || sym == "f");
-        }
     }
 }
