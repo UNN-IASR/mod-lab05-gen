33b16f336773445197a399e99e4c1540d77acf4f
marikozlova <162039344+marikozlova@users.noreply.github.com>
2024-06-09 16:55:41 +0300

    Update Program.cs

--- ProjCharGenerator/Program.cs
+++ ProjCharGenerator/Program.cs
@@ -4,72 +4,72 @@ using System.IO;
 
 namespace generator
 {
-   public class CharGenerator
+    public class Generator_char
     {
         private string syms = "абвгдёежзийклмнопростуфхцчшщъыьэюя";
         private char[] data;
         private int size;
         private Random random = new Random();
-        public CharGenerator()
+        public Generator_char()
         {
             size = syms.Length;
             data = syms.ToCharArray();
         }
-        public char getSym()
+        public char Get_symb()
         {
             return data[random.Next(0, size)];
         }
     }
 
-    public class TextGenerator
+    public class Generator_text
     {
+        private double[] sum;
         private string[] words;
-        private double[] weightSum;
 
         private Random random = new Random();
-        public TextGenerator()
+        public Generator_text()
         {
 
         }
-        public TextGenerator(string[] words, double[] values)
+        public Generator_text(string[] words, double[] values)
         {
             this.words = words;
-            weightSum = new double[words.Length];
-            weightSum[0] = values[0];
+            sum = new double[words.Length];
+            sum[0] = values[0];
             for (int i = 1; i < words.Length; i++)
             {
-                weightSum[i] = weightSum[i - 1] + values[i];
-            }
-        }
-        public void FileLoading(string filename)
-        {
-            string[] lines = File.ReadAllLines(filename);
-            words = new string[lines.Length];
-            weightSum = new double[lines.Length];
-
-            string[] parseParts = lines[0].Split(' ');
-            words[0] = parseParts[0];
-            weightSum[0] = double.Parse(parseParts[1]);
-            for (int i = 1; i < lines.Length; i++)
-            {
-                parseParts = lines[i].Split(' ');
-                words[i] = parseParts[0];
-                weightSum[i] = weightSum[i - 1] + double.Parse(parseParts[1]);
+                sum[i] = sum[i - 1] + values[i];
             }
         }
 
-        public string getSym()
+        public string Get_symb()
         {
-            int size = weightSum.Length;
-            double value = random.NextDouble() * weightSum[size - 1];
+            int size = sum.Length;
+            double value = random.NextDouble() * sum[size - 1];
             for (int i = 0; i < size; i++)
             {
-                if (value < weightSum[i])
+                if (value < sum[i])
                 {
                     return words[i];
                 }
             }
             return null;
+        }
+
+        public void Data_loading(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+            words = new string[lines.Length];
+            sum = new double[lines.Length];
+            string[] parseParts = lines[0].Split(' ');
+            words[0] = parseParts[0];
+            sum[0] = double.Parse(parseParts[1]);
+            for (int i = 1; i < lines.Length; i++)
+            {
+                parseParts = lines[i].Split(' ');
+                words[i] = parseParts[0];
+                sum[i] = sum[i - 1] + double.Parse(parseParts[1]);
+            }
         }
     }
 
@@ -77,38 +77,37 @@ namespace generator
     {
         static void Main(string[] args)
         {
-            CharGenerator gen = new CharGenerator();
+            Generator_char gen = new Generator_char();
             SortedDictionary<char, int> stat = new SortedDictionary<char, int>();
-                for (int i = 0; i < 1000; i++)
-                {
-                    char ch = gen.getSym();
-                    if (stat.ContainsKey(ch))
-                        stat[ch]++;
-                    else
-                        stat.Add(ch, 1); Console.Write(ch);
-                }
+            for (int i = 0; i < 1000; i++)
+            {
+                char ch = gen.Get_symb();
+                if (stat.ContainsKey(ch))
+                    stat[ch]++;
+                else
+                    stat.Add(ch, 1); Console.Write(ch);
+            }
             Console.Write('\n');
             foreach (KeyValuePair<char, int> entry in stat)
             {
                 Console.WriteLine("{0} - {1}", entry.Key, entry.Value / 1000.0);
             }
 
-            string resultLines1, resultLines2;
-            resultLines1 = resultLines2 = "";
-            TextGenerator generator = new TextGenerator();
-            generator.FileLoading("Data_1.txt");
+            string result_1, result_2;
+            result_1 = result_2 = "";
+            Generator_text generator = new Generator_text();
+            generator.Data_loading("Data_1.txt");
             for (int i = 0; i < 1000; i++)
             {
-                resultLines1 += generator.getSym();
+                result_1 += generator.Get_symb();
             }
-            File.WriteAllText("gen_1.txt", resultLines1, System.Text.Encoding.UTF8);
-
-            generator.FileLoading("Data_2.txt");
+            File.WriteAllText("gen_1.txt", result_1, System.Text.Encoding.UTF8);
+            generator.Data_loading("Data_2.txt");
             for (int i = 0; i < 1000; i++)
             {
-                resultLines2 += generator.getSym();
+                result_2 += generator.Get_symb();
             }
-            File.WriteAllText("gen_2.txt", resultLines2, System.Text.Encoding.UTF8);
+            File.WriteAllText("gen_2.txt", result_2, System.Text.Encoding.UTF8);
         }
     }
 }
