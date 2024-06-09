be87154830bce68cbbb80aa33dc18c5e38e4f5bc
kateandrianova <147188563+kateandrianova@users.noreply.github.com>
2024-06-09 18:58:24 +0300

    Update Test.cs

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -12,5 +12,41 @@ namespace generator
             TextGenerator generator = new TextGenerator(words, values);
             Assert.IsNotNull(generator);
         }
+        
+        [TestMethod]
+        public void TestCharGenerator()
+        {
+            CharGenerator generator = new CharGenerator();
+            char symbol = generator.getSym();
+            Assert.IsTrue("абвгдеёжзийклмнопрстуфхцчшщъыьэюя".Contains(symbol.ToString()));
+        }
+
+        [TestMethod]
+        public void TestCharGenerator2()
+        {
+            CharGenerator generator = new CharGenerator();
+            char symbol = generator.getSym();
+            Assert.IsFalse("abcdefghijklmnopqrstuvwxyz".Contains(symbol.ToString()));
+        }
+
+        [TestMethod]
+        public void TestGetSymMethod()
+        {
+            string[] words = { "a", "b", "c", "d", "e", "f" };
+            double[] values = { 0.1, 0.2, 0.3, 0.6, 0.8, 1.0 };
+            TextGenerator generator = new TextGenerator(words, values);
+
+            string sym = generator.getSym();
+            Assert.IsTrue(sym == "a" || sym == "b" || sym == "c" || sym == "d" || sym == "e" || sym == "f");
+        }
+
+        [TestMethod]
+        public void TestConstructingFromFile()
+        {
+            TextGenerator generator = new TextGenerator();
+            generator.FileLoading("Data1.txt");
+            Assert.IsNotNull(generator);
+        }
+
     }
 }
